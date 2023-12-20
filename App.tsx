c3b49905583e12dad19f381f9ade2c98cb66057e
andreypats <andrey.it.work@gmail.com>
2023-12-20 11:32:39 +0300

    Change App function, add styles

--- App.tsx
+++ App.tsx
@@ -1,22 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hi, Andrey!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [value, setValue] = useState('Text')
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={[globalStyles.border, styles.textInput]} value={value} onChangeText={setValue}/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#316515',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        width: 200,
+        backgroundColor: 'white',
+        fontSize: 18,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+    },
 });
+
+const globalStyles = StyleSheet.create({
+    border: {
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+})
+
+// 13_00 02-Вводное видео
